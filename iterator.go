97fab72f95559f6745f8c95ddcb47a732918467c
saxsir <saxsir.256+github@gmail.com>
2016-06-11 16:15:28 +0900

    大枠をざっと書いた
    - NewBookはとりあえずテスト

--- iterator.go
+++ iterator.go
@@ -2,6 +2,53 @@ package iterator
 
 import ()
 
-func Ping() string {
-	return "pong"
+// 集合体のインターフェース
+type Aggregate interface {
+	iterator()
+}
+
+// 数え上げる人のインターフェース
+type Iterator interface {
+	hasNext() bool
+	next()
+}
+
+// 本棚 - Aggregateの実装
+type BookShelf struct {
+	books []*Book
+	last  int
+}
+
+func (bs *BookShelf) iterator() *BookShelfIterator {
+	return NewBookShelfIterator()
+}
+
+// 本棚の中身を数え上げる人 - Iteratorの実装
+type BookShelfIterator struct {
+	bookShelf *BookShelf
+	index     int
+}
+
+func NewBookShelfIterator() *BookShelfIterator {
+	return &BookShelfIterator{}
+}
+func (it *BookShelfIterator) hasNext() bool {
+	return true
+}
+func (it *BookShelfIterator) next() *Book {
+	return &Book{}
+}
+
+// 本
+type Book struct {
+	name string
+}
+
+func NewBook(n string) *Book {
+	return &Book{
+		name: n,
+	}
+}
+func (b *Book) GetName() string {
+	return b.name
 }
--- iterator_test.go
+++ iterator_test.go
@@ -4,9 +4,16 @@ import (
 	"testing"
 )
 
-func TestPing(t *testing.T) {
-	r := Ping()
-	if expected := "pong"; r != expected {
-		t.Fatalf("expected %s, actual %s", expected, r)
+func check(a, e interface{}, t *testing.T) {
+	if a != e {
+		t.Errorf("expected %v, actual %v", e, a)
 	}
+}
+
+// NewBookできるかテストする
+// ちゃんと指定した名前の本ができていれば良い
+func TestNewBook(t *testing.T) {
+	e := "example"
+	b := NewBook(e)
+	check(b.GetName(), e, t)
 }
